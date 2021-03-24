88b0d82a666a717ae7d9a30de7e45efcb639e205
Abhishek S <52904509+abhisheksunil2201@users.noreply.github.com>
2021-03-24 19:39:15 +0530

    Updated instructions

--- src/API_key.js
+++ src/API_key.js
@@ -4,6 +4,7 @@ export const Apicontext = createContext();
 
 export const Apiprovider = (props) => {
   //PASTE YOUR API KEY BELOW
+  // for eg: const apikey = "asdfadf8asdf4gafsdg97asfd";
   const apikey = "";
   return (
     <Apicontext.Provider value={apikey}>{props.children}</Apicontext.Provider>
